f7746ddc76513e1bd11b5ea918dd60e35b5cf61f
Grace Lam <gracesmile88@hotmail.com>
2019-01-09 16:15:28 -0800

    add bodyparts for pinBodyPart.php

--- pinBodyPart.php
+++ pinBodyPart.php
@@ -1,7 +1,75 @@
-<?php include ("includes/header.php");?>
+<?php
+  require_once("includes/db_connection.php");
+  
+  $sql = "SELECT * FROM bodybuilders";
+  $result = $connection->query($sql);
+
+?>
+
+<?php require ("includes/header.php");?>
 
 <h2>Pin Body Part</h2>
 
+  <div class="content-wrapper">
+    <div class="main-features">
+      <a href="#">
+        <div class="site-feature">
+          <div class="pin-muscle-group">
+            <h2>Wrist</h2>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="site-feature">
+          <div class="pin-by-height">
+            <h2>Chest</h2>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+<h2>Bodybuilders</h2>
+
+  <div class="content-wrapper">
+    <div class="main-features">
+      <a href="#">
+
+<?php 
+  if ($result->num_rows > 0) {
+    // output data
+    while($row = $result->fetch_assoc()) {
+      echo "        
+      		<div class=\"site-feature\">
+          	<div class=\"pin-muscle-group\">
+          	";
+      echo "name: " . $row["name"];
+      echo "<br />";
+      echo "height: " . $row["height"] . " inch";
+      echo "</div>";
+      echo "</div>";
+      echo "<br />";
+    }
+  }
+
+  $connection->close();
+  
+?>
 
 
-<?php include ("includes/footer.php");?>
+            <h2>Wrist</h2>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="site-feature">
+          <div class="pin-by-height">
+            <h2>Chest</h2>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+
+<?php require ("includes/footer.php");?>
