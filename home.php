332a6d90333fdbdf65a05b7c25143b6c82c401d4
jayant95 <jht1995@hotmail.com>
2019-08-27 12:07:02 -0700

    Add flexbox to the homepage
    Made the homepage responsive

--- home.php
+++ home.php
@@ -5,71 +5,66 @@
 ?>
 
 <div class="main-features">
-  <a href="pin-muscle.php">
+  <div class="first-row-features">
     <div class="site-feature">
-      <div class="pin-muscle-group">
-        <div class="feature-info-img">
-          <img src="img/pinMuscleGroup.png">
-        </div>
-        <div class="feature-info">
-          <h2>Pin Muscle Group</h2>
-          <p>Satisfied with a certain muscle group? Pin it! Let your other muscle groups catch up</p>
-        </div>
-      </div>
-    </div>
-  </a>
-  <a href="pin-height.php">
-    <div class="site-feature">
-      <div class="pin-by-height">
-        <div class="feature-info-img">
+      <div class="feature-info-img by-height">
+        <a href="pin-height.php">
           <img src="img/pinHeight.png">
-        </div>
-        <div class="feature-info">
-          <h2>Height</h2>
-          <p>Choose your bodybuilder and see your ideal measurements by height</p>
-        </div>
+        </a>
+      </div>
+      <div class="feature-info">
+        <h2>Height</h2>
+        <p>Choose a bodybuilder from the list and see your ideal measurements by height</p>
       </div>
     </div>
-  </a>
-  <a href="muscle-converter.php">
     <div class="site-feature">
-      <div class="muscle-to-muscle">
-        <div class="feature-info-img">
+      <div class="feature-info-img muscle-to-muscle">
+        <a href="muscle-converter.php">
           <img src="img/muscleToMuscle.png">
-        </div>
-        <div class="feature-info">
-          <h2>Muscle to Muscle</h2>
-          <p>Compare two muscle groups using this conversion calculator with different bodybuilders</p>
-        </div>
+        </a>
+      </div>
+      <div class="feature-info">
+        <h2>Muscle to Muscle</h2>
+        <p>Compare two muscle groups using this conversion calculator with different bodybuilders</p>
       </div>
     </div>
-  </a>
-  <a href="custom.php">
+  </div>
+
+  <div class="second-row-features">
     <div class="site-feature">
-      <div class="frankenstein">
-        <div class="feature-info-img">
+      <div class="feature-info-img pin-muscle">
+        <a href="pin-muscle.php">
+          <img src="img/pinMuscleGroup.png">
+        </a>
+      </div>
+      <div class="feature-info">
+        <h2>Pin Muscle Group</h2>
+        <p>Choose a bodybuilder from the list and see your ideal measurements by height</p>
+      </div>
+    </div>
+    <div class="site-feature">
+      <div class="feature-info-img custom-feature">
+        <a href="custom.php">
           <img src="img/customBody.png">
-        </div>
-        <div class="feature-info">
-          <h2>Custom</h2>
-          <p>Create your own ideal measurements and stay on track</p>
-        </div>
+        </a>
+      </div>
+      <div class="feature-info">
+        <h2>Custom</h2>
+        <p>Create your own ideal measurements and stay on track</p>
       </div>
     </div>
-  </a>
-  <a href="golden-ratio.php">
     <div class="site-feature">
-      <div class="golden-ratio">
-        <div class="feature-info-img">
+      <div class="feature-info-img golden-ratio">
+        <a href="pin-height.php">
           <img src="img/goldenRatio.png">
-        </div>
-        <div class="feature-info">
-          <h2>Golden Ratio</h2>
-          <p>Do you have the golden ratio physique? Come and find out</p>
-        </div>
+        </a>
+      </div>
+      <div class="feature-info">
+        <h2>Golden Ratio</h2>
+        <p>Do you have the golden ratio physique? Come and find out</p>
       </div>
     </div>
-  </a>
+  </div>
 </div>
 
 
