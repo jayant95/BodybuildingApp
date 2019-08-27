d25a44c0b307df923717bdd86436d82740d98414
jayant95 <jht1995@hotmail.com>
2019-08-26 20:23:11 -0700

    Add new icons to homepage
    Created new graphics/icons for feature list.
    Added icon for each feature

--- home.php
+++ home.php
@@ -7,41 +7,66 @@
 <div class="main-features">
   <a href="pin-muscle.php">
     <div class="site-feature">
-      <div class="pin-muscle-tab">
-        <h2>Pin Muscle Group</h2>
-        <p>Satisfied with a certain muscle group? Pin it! Let your other muscle groups catch up</p>
+      <div class="pin-muscle-group">
+        <div class="feature-info-img">
+          <img src="img/pinMuscleGroup.png">
+        </div>
+        <div class="feature-info">
+          <h2>Pin Muscle Group</h2>
+          <p>Satisfied with a certain muscle group? Pin it! Let your other muscle groups catch up</p>
+        </div>
       </div>
     </div>
   </a>
   <a href="pin-height.php">
     <div class="site-feature">
       <div class="pin-by-height">
-        <h2>Height</h2>
-        <p>Choose your bodybuilder and see your ideal measurements by height</p>
+        <div class="feature-info-img">
+          <img src="img/pinHeight.png">
+        </div>
+        <div class="feature-info">
+          <h2>Height</h2>
+          <p>Choose your bodybuilder and see your ideal measurements by height</p>
+        </div>
       </div>
     </div>
   </a>
   <a href="muscle-converter.php">
     <div class="site-feature">
       <div class="muscle-to-muscle">
-        <h2>Muscle to Muscle</h2>
-        <p>Compare two muscle groups using this conversion calculator with different bodybuilders</p>
+        <div class="feature-info-img">
+          <img src="img/muscleToMuscle.png">
+        </div>
+        <div class="feature-info">
+          <h2>Muscle to Muscle</h2>
+          <p>Compare two muscle groups using this conversion calculator with different bodybuilders</p>
+        </div>
       </div>
     </div>
   </a>
   <a href="custom.php">
     <div class="site-feature">
       <div class="frankenstein">
-        <h2>Frankenstein</h2>
-        <p>Create your own ideal measurements and stay on track</p>
+        <div class="feature-info-img">
+          <img src="img/customBody.png">
+        </div>
+        <div class="feature-info">
+          <h2>Custom</h2>
+          <p>Create your own ideal measurements and stay on track</p>
+        </div>
       </div>
     </div>
   </a>
   <a href="golden-ratio.php">
     <div class="site-feature">
       <div class="golden-ratio">
-        <h2>Golden Ratio</h2>
-        <p>Do you have the golden ratio physique? Come and find out</p>
+        <div class="feature-info-img">
+          <img src="img/goldenRatio.png">
+        </div>
+        <div class="feature-info">
+          <h2>Golden Ratio</h2>
+          <p>Do you have the golden ratio physique? Come and find out</p>
+        </div>
       </div>
     </div>
   </a>
--- physique.sql
+++ physique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 31, 2019 at 12:10 AM
+-- Generation Time: Aug 18, 2019 at 10:18 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -61,32 +61,52 @@ INSERT INTO `bodybuilders` (`bodybuilderID`, `name`, `height`, `contestWeight`, 
 --
 
 CREATE TABLE `goals` (
-  `memberID` varchar(255) NOT NULL,
-  `goalID` varchar(255) NOT NULL,
-  `bodyBuilderID` varchar(255) NOT NULL,
+  `memberID` int(11) NOT NULL,
+  `goalID` int(11) NOT NULL,
+  `bodybuilderID` int(11) NOT NULL,
+  `bodybuilderName` varchar(255) NOT NULL,
   `featureName` varchar(255) NOT NULL,
-  `currentGoal` varchar(255) NOT NULL,
+  `currentGoal` tinyint(1) NOT NULL,
   `date` varchar(255) NOT NULL,
-  `leftArm` varchar(255) NOT NULL,
-  `rightArm` varchar(255) NOT NULL,
-  `chest` varchar(255) NOT NULL,
-  `waist` varchar(255) NOT NULL,
-  `leftThigh` varchar(255) NOT NULL,
-  `rightThigh` varchar(255) NOT NULL,
-  `leftCalf` varchar(255) NOT NULL,
-  `rightCalf` varchar(255) NOT NULL,
-  `shoulders` varchar(255) NOT NULL,
-  `weight` varchar(255) NOT NULL,
-  `bodyFat` varchar(255) NOT NULL
+  `arms` float NOT NULL,
+  `chest` float NOT NULL,
+  `waist` float NOT NULL,
+  `thighs` float NOT NULL,
+  `calves` float NOT NULL,
+  `shoulders` float NOT NULL,
+  `neck` float NOT NULL,
+  `weight` float NOT NULL,
+  `bodyFat` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `goals`
 --
 
-INSERT INTO `goals` (`memberID`, `goalID`, `bodyBuilderID`, `featureName`, `currentGoal`, `date`, `leftArm`, `rightArm`, `chest`, `waist`, `leftThigh`, `rightThigh`, `leftCalf`, `rightCalf`, `shoulders`, `weight`, `bodyFat`) VALUES
-('11', '11', 'Frank Zane', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-('11', '11', 'Franco Columbo', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+INSERT INTO `goals` (`memberID`, `goalID`, `bodybuilderID`, `bodybuilderName`, `featureName`, `currentGoal`, `date`, `arms`, `chest`, `waist`, `thighs`, `calves`, `shoulders`, `neck`, `weight`, `bodyFat`) VALUES
+(4, 1, 2, 'name', 'custom', 1, '123', 1, 2, 3, 4, 5, 6, 7, 8, 9),
+(29, 2, 29, 'custom', 'custom', 0, '1550006815', 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 3, 29, 'custom', 'custom', 0, '1550006818', 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 4, -1, '', 'custom', 0, '1550006913', 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 5, -1, '', 'custom', 0, '1550007447', 16, 45, 32, 25, 16, 53, 16, 180, 10),
+(29, 6, -1, '', 'custom', 0, '1550007497', 16, 45, 32, 25, 16, 53, 16, 180, 10),
+(29, 7, -1, '', 'custom', 0, '1550025131', 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 8, -1, '', 'custom', 0, '1551223500', 16, 45, 32, 25, 16, 52, 16, 180, 12),
+(29, 9, -1, '', 'custom', 0, '1551223591', 16, 45, 32, 25, 16, 52, 16, 180, 12),
+(29, 10, 0, 'Frank Zane', 'Pin by height', 0, '1551228569', 18.5217, 52.4783, 29.8406, 26.7536, 17.4928, 0, 0, 0, 0),
+(29, 11, -1, '', 'custom', 0, '1551229703', 16, 45, 32, 25, 16, 51, 16, 180, 12),
+(29, 12, 0, '', 'Golden Ratio', 0, '1551230865', 17.5, 45.5, 34, 26.25, 17.5, 55.01, 17.5, 0, 0),
+(29, 13, 0, '', 'Golden Ratio', 0, '1551308877', 17.5, 45.5, 34, 26.25, 17.5, 55.01, 17.5, 0, 0),
+(29, 14, 0, 'Arnold Schwarzenegger', 'Pin by height', 0, '1551309270', 21.1081, 54.6892, 32.6216, 27.3446, 19.1892, 0, 0, 0, 0),
+(29, 15, 0, 'Serge Nubret', 'Pin by leftArm', 0, '1551309367', 15, 39.7674, 19.186, 18.8372, 13.2558, 0, 0, 0, 0),
+(29, 16, 0, 'Lee Haney', 'Pin by height', 0, '1551309391', 21.3496, 55.6084, 31.2797, 29.7902, 19.8601, 0, 0, 0, 0),
+(29, 17, 0, '', 'Golden Ratio', 0, '1551309409', 17.5, 45.5, 34, 26.25, 17.5, 55.01, 17.5, 0, 0),
+(29, 18, 0, 'Arnold Schwarzenegger', 'Pin by leftArm', 1, '1552110590', 15, 38.8636, 23.1818, 19.4318, 13.6364, 0, 0, 0, 0),
+(33, 19, 0, 'Serge Nubret', 'Pin by leftArm', 0, '1558123808', 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(33, 20, 0, 'Frank Zane', 'Pin by height', 0, '1558124055', 18.5217, 52.4783, 29.8406, 26.7536, 17.4928, 0, 0, 0, 0),
+(33, 21, -1, '', 'custom', 0, '1558124311', 18.5217, 52.4783, 29.8406, 26.7536, 17.4928, 50, 16, 180, 12),
+(33, 22, 0, 'Franco Columbo', 'Pin by height', 0, '1558124344', 20.7538, 54.6154, 32.7692, 28.4, 19.1154, 0, 0, 0, 0),
+(33, 23, -1, '', 'custom', 1, '1558124381', 17, 44, 32, 24, 16, 50, 16, 180, 12);
 
 -- --------------------------------------------------------
 
@@ -111,6 +131,19 @@ CREATE TABLE `memberlog` (
   `bodyFat` float NOT NULL,
   `neck` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `memberlog`
+--
+
+INSERT INTO `memberlog` (`measurementID`, `memberID`, `timestamp`, `leftArm`, `rightArm`, `chest`, `waist`, `leftThigh`, `rightThigh`, `leftCalf`, `rightCalf`, `shoulders`, `weight`, `bodyFat`, `neck`) VALUES
+(36, 30, '1548891223', 15, 15, 4, 88, 4, 9, 9, 9, 9, 0, 0, 2),
+(37, 30, '1548891260', 15, 15, 4, 88, 4, 9, 9, 9, 9, 0, 0, 2),
+(38, 30, '1548892231', 15, 15, 42, 34, 23, 23, 15, 15, 51, 185, 15, 14),
+(39, 29, '1549323236', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(40, 29, '1549323273', 15, 15, 42.25, 34, 23, 23, 15, 15, 51, 185, 15, 16),
+(41, 29, '1551309991', 15, 15, 42.25, 32, 23, 23, 15, 15, 51, 185, 15, 16),
+(42, 33, '1558124035', 15, 15, 42, 34, 23, 23, 15, 15, 50, 190, 15, 16);
 
 -- --------------------------------------------------------
 
@@ -139,15 +172,40 @@ CREATE TABLE `members` (
   `bodyFat` float NOT NULL,
   `weight` float NOT NULL,
   `neck` float NOT NULL,
-  `knee` float NOT NULL
+  `knee` float NOT NULL,
+  `height` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`memberID`, `firstName`, `lastName`, `email`, `username`, `password`, `leftArm`, `rightArm`, `chest`, `waist`, `leftThigh`, `rightThigh`, `leftCalf`, `rightCalf`, `shoulders`, `wrists`, `ankles`, `bodyFat`, `weight`, `neck`, `knee`) VALUES
-(26, 'Jayant', 'Tailor', 'jayantt@sfu.ca', 'jayant95', '$2y$10$P96sqPD5z5IusgVb/Daj2uMa.3EyGpR56EZvT8zRhPhGyaWXIfL1S', 16, 16.5, 41, 34, 23, 23, 16, 16, 51, 6, 7, 15, 185, 16, 15);
+INSERT INTO `members` (`memberID`, `firstName`, `lastName`, `email`, `username`, `password`, `leftArm`, `rightArm`, `chest`, `waist`, `leftThigh`, `rightThigh`, `leftCalf`, `rightCalf`, `shoulders`, `wrists`, `ankles`, `bodyFat`, `weight`, `neck`, `knee`, `height`) VALUES
+(28, 'rick', 'morty', 'rock@r.com', 'rick01', '$2y$10$4j3fQxrcxAWNoFmsC6hbiuJtGLWMO1fIZl02aRb3jL/CEAYL2t4Z.', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 'Jayant', 'Tailor', 'jayantt@sfu.ca', 'jayant95', '$2y$10$J8C30PSkL3LRQ5E8e5Kvgu.X9Z2RTvhMK8A8IUa0s5uDHhtgiq0I2', 15, 15, 42.25, 32, 23, 23, 15, 15, 51, 7, 8, 15, 185, 16, 15, 71),
+(30, 'Bob', 'Smith', 'bob@smith.com', 'bobby01', '$2y$10$sNGH1Ynn.0G12UDQPq3aGO4j3G0e3nRpXjOdxdG2NuD6KDTIVPTTe', 15, 15, 42, 34, 23, 23, 15, 15, 51, 6.5, 6.5, 15, 185, 14, 15, 0),
+(33, 'chef', 'boy', 'chefboy@hotmail.com', 'chefboy', '$2y$10$qEPcPkqq2RR0PXInNJwwR.0xYMQBzoKJvTdn0fHuhm2AnZ8YVZm6a', 15, 15, 42, 34, 23, 23, 15, 15, 50, 7, 7, 15, 190, 16, 7, 71),
+(34, 'bob', 'joe', 'bob@j.com', 'bobbyJ', '$2y$10$qH/ipesh1Ud6EreSHmk2Fe6cB8KHRmzCBZdx3TTuJK9Kr5eErCfwC', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL,
+  `memberID` int(11) NOT NULL,
+  `unit` varchar(255) NOT NULL,
+  `privacy` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `memberID`, `unit`, `privacy`) VALUES
+(2, 34, 'Inches', 'Private');
 
 --
 -- Indexes for dumped tables
@@ -158,6 +216,12 @@ INSERT INTO `members` (`memberID`, `firstName`, `lastName`, `email`, `username`,
 --
 ALTER TABLE `bodybuilders`
   ADD PRIMARY KEY (`bodybuilderID`);
+
+--
+-- Indexes for table `goals`
+--
+ALTER TABLE `goals`
+  ADD PRIMARY KEY (`goalID`);
 
 --
 -- Indexes for table `memberlog`
@@ -172,6 +236,12 @@ ALTER TABLE `members`
   ADD PRIMARY KEY (`memberID`);
 
 --
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -182,16 +252,28 @@ ALTER TABLE `bodybuilders`
   MODIFY `bodybuilderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `goals`
+--
+ALTER TABLE `goals`
+  MODIFY `goalID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT for table `memberlog`
 --
 ALTER TABLE `memberlog`
-  MODIFY `measurementID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `measurementID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `memberID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `memberID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
