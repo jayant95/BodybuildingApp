96536211b8d894af21d8714d664e34ae3fea955d
jayant95 <jht1995@hotmail.com>
2019-09-11 10:16:25 -0700

    Revamp register.php
    Same form design for login was added for registering

--- register.php
+++ register.php
@@ -53,11 +53,58 @@
   }
 
 ?>
+<div class="login-wrapper">
+  <div class="container-login background-image overlay">
+			<div class="wrap-login">
+				<form class="login-form" action<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post">
+          <h3 class="login-header">Register</h3>
 
 
+					<div class="wrap-input">
+          <label class="input-label">First Name</label>
+						<input class="form-input" type="text" name="first-name" placeholder="Bob" value="<?php echo $user['first-name'] ?>">
+					</div>
+
+					<div class="wrap-input">
+            <label class="input-label">Last Name</label>
+						<input class="form-input" type="text" name="last-name" placeholder="Smith" value="<?php echo $user['last-name'] ?>">
+          </div>
+          
+          <div class="wrap-input">
+            <label class="input-label">Email</label>
+						<input class="form-input" type="text" name="email" placeholder="abc@abc.com" value="<?php echo $user['email'] ?>">
+          </div>
+          
+          <div class="wrap-input">
+            <label class="input-label">Username</label>
+						<input class="form-input" type="text" name="username" placeholder="Bobby007" value="<?php echo $user['username'] ?>">
+					</div>
+					
+          <div class="wrap-input">
+            <label class="input-label">Password</label>
+            <label class="input-label small">Minimum of 8 characters. At least one captial letter, lowercase letter, special character and number</label>
+            <input class="form-input" type="password" name="password" value="" placeholder="">
+          </div>
+
+          <div class="wrap-input">
+            <label class="input-label">Confirm Password</label>
+            <input class="form-input" type="password" name="confirm-password" value="" placeholder="">
+          </div>
+          
+          <div class="login-form-btn">
+            <input class="login-button" type="submit" name="submit" value="Submit">
+					</div>
+
+					<div class="login-form-link">
+            <p>Have an account? <a class="register-link" href="login.php">Sign in here</a></p>
+					</div>
+				</form>
+			</div>
+  </div>
+</div>
 
 
-<div class="content-wrapper">
+<!-- <div class="content-wrapper">
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <div class="register-form">
       <label>First Name:</label>
@@ -75,7 +122,7 @@
       <input class="login-button" type="submit" name="submit" value="Submit"/>
     </div>
   </form>
-</div>
+</div> -->
 
 
 <?php require("includes/footer.php") ?>
