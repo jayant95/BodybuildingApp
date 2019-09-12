fabe4a263746ca3fb3130cc188378f930a840a84
jayant95 <jht1995@hotmail.com>
2019-09-12 09:48:46 -0700

    Design custom.php page
    Used CSS classes to style this page. Took css classes from
    login/register and extended them for further customization

--- custom.php
+++ custom.php
@@ -44,81 +44,91 @@
 	}
 ?>
 
-<div class="content-wrapper">
-
-
-	<div class="current-stats">
-	<h2>Current Stats</h2>
-	<p>Chest: <?php echo $userStats['chest']  ?></p>
-	<p>Shoulders: <?php echo $userStats['shoulders']  ?></p>
-	<p>Neck: <?php echo $userStats['neck']  ?></p>
-	<p>Left Arm: <?php echo $userStats['leftArm']  ?></p>
-	<p>Right Arm: <?php echo $userStats['rightArm']  ?></p>
-	<p>Waist: <?php echo $userStats['waist']  ?></p>
-	<p>Left Thigh: <?php echo $userStats['leftThigh']  ?></p>
-	<p>Right Thigh: <?php echo $userStats['rightThigh']  ?></p>
-	<p>Left Calf: <?php echo $userStats['leftCalf']  ?></p>
-	<p>Right Calf: <?php echo $userStats['rightCalf']  ?></p>
-	</div>
-
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-		<h2>Current Goal</h2>
-		<div class="custom-stats">
-			<div class="custom-stat-group">
-				<label>Type:</label>
-				<?php echo $userGoal ? "<p>" . $userGoal['featureName'] . "</p>" : "<p>N/A</p>"; ?>
+<div class="page-background">
+	<div class="content-wrapper">
+		<div class="user-stats current">
+			<h2>Current Stats</h2>
+			<div class="stats-group left">
+				<p>Chest: <?php echo $userStats['chest']  ?></p>
+				<p>Shoulders: <?php echo $userStats['shoulders']  ?></p>
+				<p>Neck: <?php echo $userStats['neck']  ?></p>
+				<p>Left Arm: <?php echo $userStats['leftArm']  ?></p>
+				<p>Right Arm: <?php echo $userStats['rightArm']  ?></p>
 			</div>
-				<?php
-					if ($userGoal) {
-						if ($userGoal['bodybuilder'] != NULL) {
-							echo "<div class='custom-stat-group'>";
-							echo 		"<label>Bodybuilder:</label>";
-							echo 		"<p>" . $userGoal['bodybuilder'] . "</p>";
-							echo "</div>";
-						}
-					}
-				?>
-			<div class="custom-stat-group">
-				<label>Chest:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='chest' value=". $userGoal['chest'] .">" : "<p>" . $userGoal['chest'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Shoulders:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='shoulders' value=". $userGoal['shoulders'] .">" : "<p>" . $userGoal['shoulders'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Neck:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='neck' value=". $userGoal['neck'] .">" : "<p>" . $userGoal['neck'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Arms:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='arms' value=". $userGoal['arms'] .">" : "<p>" . $userGoal['arms'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Waist:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='waist' value=". $userGoal['waist'] .">" : "<p>" . $userGoal['waist'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Thighs:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='thighs' value=". $userGoal['thighs'] .">" : "<p>" . $userGoal['thighs'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Calves:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='calves' value=". $userGoal['calves'] .">" : "<p>" . $userGoal['calves'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Weight:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='weight' value=". $userGoal['weight'] .">" : "<p>" . $userGoal['weight'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<label>Body Fat:</label>
-				<?php echo $editActive ? "<input class='profile-input' type='number' step='any' name='bodyfat' value=". $userGoal['bodyFat'] .">" : "<p>" . $userGoal['bodyFat'] . "</p>"; ?>
-			</div>
-			<div class="custom-stat-group">
-				<?php echo $editActive ? "<input class='profile-input' type='submit' name='update' value='Update Goal'>" : "<input class='profile-input' type='submit' name='edit' value='Edit Goal'>"; ?>
+			<div class="stats-group right">
+				<p>Waist: <?php echo $userStats['waist']  ?></p>
+				<p>Left Thigh: <?php echo $userStats['leftThigh']  ?></p>
+				<p>Right Thigh: <?php echo $userStats['rightThigh']  ?></p>
+				<p>Left Calf: <?php echo $userStats['leftCalf']  ?></p>
+				<p>Right Calf: <?php echo $userStats['rightCalf']  ?></p>
 			</div>
 		</div>
-	</form>
+
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+			<div class="user-stats">
+				<h2>Current Goal</h2>
+				<div class="custom-stats">
+					<div class="stats-group left">
+						<div class="custom-stat-group">
+							<label>Type:</label>
+							<?php echo $userGoal ? "<p>" . $userGoal['featureName'] . "</p>" : "<p>N/A</p>"; ?>
+						</div>
+							<?php
+								if ($userGoal) {
+									if ($userGoal['bodybuilder'] != NULL) {
+										echo "<div class='custom-stat-group'>";
+										echo 		"<label>Bodybuilder:</label>";
+										echo 		"<p>" . $userGoal['bodybuilder'] . "</p>";
+										echo "</div>";
+									}
+								}
+							?>
+						<div class="custom-stat-group">
+							<?php echo $editActive ? "<label class='input-label'>Chest:</label>" : "<label>Chest:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='chest' value=". $userGoal['chest'] .">" : "<p>" . $userGoal['chest'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+							<?php echo $editActive ? "<label class='input-label'>Shoulders:</label>" : "<label>Shoulders:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='shoulders' value=". $userGoal['shoulders'] .">" : "<p>" . $userGoal['shoulders'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+						<?php echo $editActive ? "<label class='input-label'>Neck:</label>" : "<label>Neck:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='neck' value=". $userGoal['neck'] .">" : "<p>" . $userGoal['neck'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+							<?php echo $editActive ? "<label class='input-label'>Arms:</label>" : "<label>Arms:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='arms' value=". $userGoal['arms'] .">" : "<p>" . $userGoal['arms'] . "</p>"; ?>
+						</div>
+					</div>
+					<div class="stats-group right">
+						<div class="custom-stat-group">
+							<?php echo $editActive ? "<label class='input-label'>Waist:</label>" : "<label>Waist:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='waist' value=". $userGoal['waist'] .">" : "<p>" . $userGoal['waist'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+							<?php echo $editActive ? "<label class='input-label'>Thighs:</label>" : "<label>Thighs:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='thighs' value=". $userGoal['thighs'] .">" : "<p>" . $userGoal['thighs'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+							<?php echo $editActive ? "<label class='input-label'>Calves:</label>" : "<label>Calves:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='calves' value=". $userGoal['calves'] .">" : "<p>" . $userGoal['calves'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+						<?php echo $editActive ? "<label class='input-label'>Weight:</label>" : "<label>Weight:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='weight' value=". $userGoal['weight'] .">" : "<p>" . $userGoal['weight'] . "</p>"; ?>
+						</div>
+						<div class="custom-stat-group">
+						<?php echo $editActive ? "<label class='input-label'>Body Fat:</label>" : "<label>Body Fat:</label>"?>
+							<?php echo $editActive ? "<input class='form-input' type='number' step='any' name='bodyfat' value=". $userGoal['bodyFat'] .">" : "<p>" . $userGoal['bodyFat'] . "</p>"; ?>
+						</div>
+					</div>
+					<div class="custom-stat-group custom-btn">
+						<?php echo $editActive ? "<input class='login-button' type='submit' name='update' value='Update Goal'>" : "<input class='login-button' type='submit' name='edit' value='Edit Goal'>"; ?>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
 </div>
 
 
